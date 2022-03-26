4a08bd398d9fae0b1cb81d6f6b79709322c79ae9
Tom Beynon <tom@tombeynon.co.uk>
2022-03-26 15:06:34 +0000

    Dynamic redis config

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ import ProxyController from './proxy/proxyController.js'
 import StatusController from './status/statusController.js'
 
 (async () => {
+  const REDIS_HOST = process.env.REDIS_HOST || 'redis'
+  const REDIS_PORT = process.env.REDIS_PORT || 6379
+
   const client = createClient({
-    url: 'redis://redis:6379'
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
   });
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
