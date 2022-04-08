4844251c254955b891f980c2757c8f9d18d429a2
Tom Beynon <tom@tombeynon.co.uk>
2022-04-08 14:08:35 +0100

    Add Bugsnag

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ import ProxyController from './proxy/proxyController.js'
 import StatusController from './status/statusController.js'
 import { redisClient } from "./redisClient.js";
 import Router from "koa-router";
+import Bugsnag from "@bugsnag/js"
+import BugsnagPluginKoa from "@bugsnag/plugin-koa"
 
 (async () => {
   const client = await redisClient();
@@ -16,6 +18,21 @@ import Router from "koa-router";
   const port = process.env.PORT || 3000;
   const app = new Koa();
   const subdomain = new Subdomain();
+
+  if(process.env.BUGSNAG_KEY){
+    Bugsnag.start({
+      apiKey: process.env.BUGSNAG_KEY,
+      plugins: [BugsnagPluginKoa],
+      enabledReleaseStages: ['production', 'staging'],
+      releaseStage: process.env.NODE_ENV
+    })
+    const middleware = Bugsnag.getPlugin('koa')
+
+    // This must be the first piece of middleware in the stack.
+    // It can only capture errors in downstream middleware
+    app.use(middleware.requestHandler)
+    app.on('error', middleware.errorHandler)
+  }
 
   app.use(cors());
 
