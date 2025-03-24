7f5acb9887fd653e65d66ca3eda5cd6f70c6f7f2
Tom Beynon <tom@tombeynon.co.uk>
2025-03-24 15:23:21 +0000

    Prevent staking rewards nomic mismatch and remove service

--- services/servicesMonitor.js
+++ services/servicesMonitor.js
@@ -164,11 +164,17 @@ function ServicesMonitor() {
       await Promise.all([...chains].map((chain) => {
         const request = async () => {
           try {
-            const asset = assets.find(el => chain.symbol && el.symbol === chain.symbol.toUpperCase())
+            let asset
+            if(!['nomic'].includes(chain.path)){
+              asset = assets.find(el => chain.symbol && el.symbol === chain.symbol.toUpperCase())
+            }
+            const validators = await client.json.get('validators:' + chain.path, '$')
             if (asset) {
               await client.json.set('chains:' + chain.path, '$', {}, { NX: true });
               await client.json.set('chains:' + chain.path, '$.services', {}, { NX: true });
               await client.json.set('chains:' + chain.path, '$.services.staking_rewards', asset);
+              if(!validators?.validators) return
+
               const providerResponse = await got.post('https://api.stakingrewards.com/public/query', { ...opts, json:
                 {
                   query: `
@@ -188,9 +194,6 @@ function ServicesMonitor() {
                 }
               }).json()
               const providers = providerResponse.data.rewardOptions
-              const validators = await client.json.get('validators:' + chain.path, '$')
-              if(!validators?.validators) return
-
               const calls = Object.entries(validators.validators).map(([address, validator]) => {
                 return async () => {
                   const assetProvider = providers.find(provider => {
@@ -211,6 +214,15 @@ function ServicesMonitor() {
               debugLog(chain.path, 'Staking Rewards update complete')
             } else {
               debugLog(chain.path, 'Staking Rewards asset not found')
+              await client.json.del('chains:' + chain.path, '$.services.staking_rewards');
+              if(!validators?.validators) return
+
+              const calls = Object.entries(validators.validators).map(([address, validator]) => {
+                return async () => {
+                  await client.json.del('validators:' + chain.path, `$.validators.${address}.services.staking_rewards`);
+                }
+              })
+              await executeSync(calls, 20)
             }
           } catch (e) { timeStamp(chain.path, 'Staking Rewards check failed', e.message) }
         };
